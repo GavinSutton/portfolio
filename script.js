1bb4ce078072cd5915112a8e7e7c1ca135f68c15
Gavin Sutton <gavin.w.c.sutton@gmail.com>
2020-05-13 16:48:23 -0400

    Added some JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("connected")
 
+const portfolio = {}
 
 // IDEAS
 
@@ -7,3 +8,21 @@ console.log("connected")
 // add konami code
 // fade in icons
 // change portfolio into cool images
+
+portfolio.init = function () {
+    $(`.toggleCredits`).on(`click`, function(){
+        $(`.credits`).toggleClass(`hide`)
+        // alert(`you clicked`)
+    })
+
+    $(`.skillsList li`).mouseenter(function(){
+        $(this).addClass(`yellow`)
+        // alert(`hovered`)
+    }).mouseleave(function(){
+        $(this).removeClass(`yellow`)
+    })
+}
+
+$(document).ready(function () {
+    portfolio.init();
+});
