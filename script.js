75173d5dcb078780a1826758814e1058401193de
Gavin Sutton <gavin.w.c.sutton@gmail.com>
2020-05-14 10:01:57 -0400

    Making them png icons turn yellow

--- script.js
+++ script.js
@@ -21,6 +21,28 @@ portfolio.init = function () {
     }).mouseleave(function(){
         $(this).removeClass(`yellow`)
     })
+
+
+    $(`.iconSwap`).mouseenter(function () {
+        // const icon = $(this.firstElementChild).attr(`src`)
+        // console.log(icon)
+        if ($(this.firstElementChild).attr(`src`) === "assets/icons/jQuery.png"){
+            $(`.jQueryIcon`).attr('src', "assets/icons/jQueryYellow.png")
+        } else if ($(this.firstElementChild).attr(`src`) === "assets/icons/firebase.png") {
+            $(`.firebaseIcon`).attr('src', "assets/icons/firebaseYellow.png")
+        } else if ($(this.firstElementChild).attr(`src`) === "assets/icons/responsive.png"){
+            $(`.responsiveIcon`).attr('src', "assets/icons/responsiveYellow.png")
+        } else if (){
+
+        }
+        // $(`.jQueryIcon`).attr('src', "assets/icons/jQueryYellow.png")
+        // alert(`hovered`)
+    }).mouseleave(function () {
+        $(`.jQueryIcon`).attr('src', "assets/icons/jQuery.png")
+        $(`.firebaseIcon`).attr('src', "assets/icons/firebase.png")
+        $(`.responsiveIcon`).attr('src', "assets/icons/responsive.png")
+        $(`.vsCodeIcon`).attr('src', "assets/icons/responsive.png")
+    })
 }
 
 $(document).ready(function () {
